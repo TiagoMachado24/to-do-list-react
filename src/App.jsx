95984ec05161d248ef2396c9a495b526e1495dbe
TiagoMachado24 <tiago.mebm.24.07@gmail.com>
2024-08-03 21:29:54 +0100

    Added handleTaskDeletion function

--- src/App.jsx
+++ src/App.jsx
@@ -38,15 +38,23 @@ const App = () => {
           completed: false,
         },
       ];
-
       setTasks(newTasks);
     }
+    
+    const handleTaskDeletion = (taskId) => {
+      const newTasks = tasks.filter((task) => task.id !== taskId);
+
+      setTasks(newTasks);
+    };
 
   return (
     <>
       <div className="container">
         <AddTask handleTaskAddition={handleTaskAddition}  />
-        <Tasks tasks={tasks} handleTaskClick={handleTaskClick}/>
+        <Tasks 
+          tasks={tasks} 
+          handleTaskClick={handleTaskClick} 
+          handleTaskDeletion={handleTaskDeletion}/>
       </div>
     </>
   )
